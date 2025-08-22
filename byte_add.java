8e142130c6ec8640311e311cc5dab8a61df268f8
Soloman Antony <solomanantony123@gmail.com>
2025-08-22 15:03:11 +0530

    Add files via upload

---
+++ byte_add.java
@@ -0,0 +1,10 @@
+public class byte_add {
+    public static void main(String[] args) {
+        byte a,b,c;
+        a=28;
+        b=32;
+        c= (byte)(a+b);
+        System.out.println("byte addition:\t"+c);
+
+    }
+}
