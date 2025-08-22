d72821b5c743c29f4994f621bf34e075accb0cfb
Soloman Antony <solomanantony123@gmail.com>
2025-08-22 15:05:52 +0530

    Add files via upload

---
+++ short_subtract.java
@@ -0,0 +1,9 @@
+public class short_subtract {
+    public static void main(String[] args) {
+        short a,b,c;
+        a=3400;
+        b=456;
+        c=(short)(a-b);
+        System.out.println("short subtraction:\t"+c);
+}
+}
