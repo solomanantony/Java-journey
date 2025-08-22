b3c248d938c2e50a04da9621b28dfa5ca6907c54
Soloman Antony <solomanantony123@gmail.com>
2025-08-22 15:06:33 +0530

    Add files via upload

---
+++ long_division.java
@@ -0,0 +1,9 @@
+public class long_division {
+    public static void main(String[] args) {
+        long a,b,c;
+        a=5657800;
+        b=56;
+        c=a/b;
+        System.out.println("long division:\t"+c);
+    }
+}
