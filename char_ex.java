cc0e562240bcf4b59155e736427d6aa6bfb2d8fd
Soloman Antony <solomanantony123@gmail.com>
2025-08-22 15:07:13 +0530

    Add files via upload

---
+++ char_ex.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class char_ex {
+    public static void main(String[]args){
+        Scanner scan=new Scanner(System.in);
+        System.out.println("enter your single character");
+        char d=scan.nextLine();
+    }    
+}
