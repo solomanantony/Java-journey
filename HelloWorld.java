34739c7a1a886135704c55cb6f71f49ea38cf0dd
Soloman Antony <solomanantony123@gmail.com>
2025-08-22 15:01:09 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,5 @@
+public class HelloWorld { //Defines a class named HelloWorld. In Java, every program starts with a class.
+    public static void main(String[] args) { //This is the entry point of any Java application. The JVM looks for this method to start execution.
+        System.out.println("Hello, World!");//Prints the text to the console.
+    }
+}
