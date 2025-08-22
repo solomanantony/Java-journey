8be282f56d7ac89340f069e0992b987eb22cc322
Soloman Antony <solomanantony123@gmail.com>
2025-08-22 15:04:45 +0530

    Add files via upload

---
+++ FloatAdd.java
@@ -0,0 +1,10 @@
+public class FloatAdd {
+    public static void main(String[] args) {
+        float a,b,c;
+        a=10;
+        b=5;
+        c=a+b;
+        System.out.println("float addtion:\t"+c);
+    }
+    
+}
