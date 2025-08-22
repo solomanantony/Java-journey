fc742da93aaa0bbd2a16d99cd39a3059bf41e021
Soloman Antony <solomanantony123@gmail.com>
2025-08-22 15:02:05 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,11 @@
+//package com.example;
+public class add {
+
+    public static void main(String[] args) {
+        int a,b,c;
+        a=45;
+        b=67;
+        c=a+b;
+        System.out.println("Addition of two number:\t"+c);
+    }
+}
