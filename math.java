b2706dcbb7fdeb77233d6eb433ad4ee3dc4d9034
Soloman Antony <solomanantony123@gmail.com>
2025-08-24 17:31:38 +0530

    Add files via upload

---
+++ math.java
@@ -0,0 +1,13 @@
+public class math {
+    public static void main(String[] args) {
+        
+        double pi= Math.PI;
+        double power= Math.pow(2, 3);
+        double absolute= Math.abs(-5);
+        double squareRoot= Math.sqrt(225);
+        double round= Math.round(3.4);
+        double ceil= Math.ceil(3.2);
+        double floor= Math.floor(3.9);
+        System.out.println(pi+ "\n"+power+"\n"+absolute+"\n"+squareRoot+"\n"+round+"\n"+ceil+"\n"+floor);
+    }
+}
